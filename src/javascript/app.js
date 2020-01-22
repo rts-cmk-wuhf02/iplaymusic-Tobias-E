6ce0a247c606feb1ab0dc37f368faefd78a0ce7f
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-01-22 17:08:52 +0100

    swiper slider added to playlists

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const params = new URLSearchParams(window.location.search);
-    console.log(params)
-    const page = params.toString("/");
+    const page = params.toString();
     console.log(page)
+
+    
 });
