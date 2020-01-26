c2ee4cc7a0fe6f50825f966217c79b9b7a1f0458
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-01-26 21:09:47 +0100

    added color change on categories

--- src/javascript/categories.js
+++ src/javascript/categories.js
@@ -14,8 +14,11 @@ document.addEventListener("DOMContentLoaded", function(){
         });
     });
 
-
-//
+// Different background color on each category
+    const colors = ['#D70060', '#E54028', '#F18D05', '#F2BC06', '#5EB11C', '#3A7634', '#0ABEBE', '#00A1CB', '#115793'];
+    arrayMore.forEach(e => {
+        e.style.background = colors[arrayMore.indexOf(e) % 9]
+    });
 });
 
 
