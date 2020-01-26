c3f216b1620c51c22e2f4d191a7b919f523ebe8d
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-01-26 16:10:14 +0100

    categories hide/show subcategories function added

--- src/javascript/categories.js
+++ src/javascript/categories.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
-    const more = document.querySelectorAll(".categoriesContainer__category");
 
-    Array.from(more).forEach(link => {
-        link.addEventListener('click', function(){
-            console.log('hello!')
-        })
-    })
+// Hide/show sub categories
+    let arrayMore = Array.from(document.querySelectorAll('.categoriesContainer__category'));
+    let arraySub = Array.from(document.querySelectorAll('.categoriesContainer__subCategories'));
+    
+    arrayMore.forEach(category => {
+        category.addEventListener('click', function(){
+            if(arraySub[arrayMore.indexOf(category)].classList.contains('hide')){
+                arraySub[arrayMore.indexOf(category)].classList.remove('hide')
+            } else{
+                arraySub[arrayMore.indexOf(category)].classList.add('hide')
+            }
+        });
+    });
+
+
+//
 });
+
+
+
